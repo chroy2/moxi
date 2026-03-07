cd1daadb567e5b722afbd8321e22c8e04030457b
chroy2 <troykwitmer@gmail.com>
2026-03-07 14:07:15 -0500

    i2c twim, libscd added

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,26 @@
 use defmt::info;
 use defmt_rtt as _;
 use embassy_executor::Spawner;
+use embassy_nrf::{
+    bind_interrupts,
+    peripherals::TWISPI0,
+    twim::{self, Twim},
+};
 use panic_probe as _;
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     info!("Starting...");
+    let p = embassy_nrf::init(Default::default());
+    bind_interrupts!(struct Irqs {
+        TWISPI0 => twim::InterruptHandler<TWISPI0>;
+    });
+    let i2c = Twim::new(
+        p.TWISPI0,
+        Irqs,
+        p.P1_00,
+        p.P0_26,
+        Default::default(),
+        &mut [], // empty ram buf
+    );
 }
