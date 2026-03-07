ef8f1b0d73c8cb3d6f614aaba2c5d82ba8e8fe80
chroy2 <troykwitmer@gmail.com>
2026-03-07 15:34:47 -0500

    adding a level indicator on the display, 400 - 1400

--- src/display.rs
+++ src/display.rs
@@ -1,7 +1,10 @@
 use core::fmt::Write;
-use embassy_time::Timer;
+use embassy_time::{Duration, Timer};
 use heapless::String;
-use microbit_bsp::{display::LedMatrix, embassy_nrf::gpio::Output};
+use microbit_bsp::{
+    display::{Bitmap, Frame, LedMatrix},
+    embassy_nrf::gpio::Output,
+};
 
 use crate::sense;
 
@@ -18,5 +21,18 @@ pub async fn display_task(mut matrix: LedMatrix<Output<'static>, ROWS, COLS>) {
         matrix.scroll(txt.as_str()).await;
         txt.clear();
         Timer::after_millis(2000).await;
+
+        let mut level = [Bitmap::empty(COLS); ROWS];
+        for (i, row) in level.iter_mut().rev().enumerate() {
+            if co2 as usize > (400 + 200 * i) {
+                *row = Bitmap::new(0b0001_1111, COLS);
+            } else {
+                break;
+            }
+        }
+
+        matrix
+            .display(Frame::new(level), Duration::from_millis(3000))
+            .await;
     }
 }
